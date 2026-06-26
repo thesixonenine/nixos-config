61863ada803c4e8efa9a406fa71501eadabdd3cd
Simple <thesixonenine@outlook.com>
2026-06-26 17:05:03 +0800

    u

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -5,8 +5,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos";
-  networking.proxy.default = "http://192.168.137.1:1080/";
-  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # networking.proxy.default = "http://192.168.137.1:1080/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   networking.networkmanager.enable = true;
 
   time.timeZone = "Asia/Shanghai";
--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }:
 
 {
+  # Hyprland Cachix cache for pre-built binaries
+  nix.settings = {
+    extra-substituters = [ "https://hyprland.cachix.org" ];
+    extra-trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    # extra-trusted-substituters = [ "https://hyprland.cachix.org" ];
+    # extra-trusted-users = [ "root" "@wheel" ];
+  };
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
