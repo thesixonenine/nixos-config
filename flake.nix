c20c7d666497f1be0c897f97342d746158aac510
Simple <thesixonenine@outlook.com>
2026-07-13 11:58:28 +0800

    u

--- flake.nix
+++ flake.nix
@@ -1,26 +1,27 @@
 {
   description = "NixOS + Hyprland + Noctalia";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    noctalia = {
-      url = "github:noctalia-dev/noctalia";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    noctalia.url = "github:noctalia-dev/noctalia/cachix";
   };
-
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [
+      "https://hyprland.cachix.org"
+      "https://noctalia.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+    extra-trusted-substituters = [ "https://hyprland.cachix.org" "https://noctalia.cachix.org" ];
+  };
   outputs = inputs@{ self, nixpkgs, noctalia, ... }: {
     # Define a system called "nixos"
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = {
-        inherit inputs;
-      };
-      modules = [
-        ./configuration.nix
-      ];
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
     };
     # You can define many systems in one Flake file.
     # NixOS will choose one based on your hostname.
--- modules/noctalia.nix
+++ modules/noctalia.nix
@@ -2,23 +2,12 @@
 
 {
   hardware.bluetooth.enable = true;
-
   services.upower.enable = true;
-
   services.power-profiles-daemon.enable = true;
 
   environment.systemPackages = [
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
 
-    extra-substituters = [ "https://noctalia.cachix.org" ];
-    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-    extra-trusted-substituters = [ "https://noctalia.cachix.org" ];
-  };
 }
